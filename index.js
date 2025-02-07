9511b364a3914b224cf166ba2ab9054f1beba0dd
DipakVyavahre <vyavahared7@gmail.com>
2025-02-07 18:56:57 +0530

    project updated

--- index.js
+++ index.js
@@ -1,2 +1,6 @@
-console.log("hello world")
-console.log("first trigger")
+const http = require('http');
+const server = http.createServer((req, res) => {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Hello, Jenkins!\n');
+});
+server.listen(3000, () => console.log('Server running on port 3000'));
